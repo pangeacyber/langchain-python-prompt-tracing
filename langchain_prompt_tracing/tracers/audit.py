5ad8a89232a237f3429043d4c17586b9a092ab76
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-09-25 16:29:43 -0400

    feat: initial commit

---
+++ langchain_prompt_tracing/tracers/audit.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from typing import Any, override
+
+from langchain_core.tracers.base import BaseTracer
+from langchain_core.tracers.schemas import Run
+from pangea import PangeaConfig
+from pangea.services import Audit
+from pangea.services.audit.util import canonicalize_json
+from pydantic import SecretStr
+
+__all__ = ["PangeaAuditCallbackHandler"]
+
+
+class PangeaAuditCallbackHandler(BaseTracer):
+    """
+    Tracer that creates an event in Pangea's Secure Audit Log when a prompt is
+    received.
+    """
+
+    _client: Audit
+
+    def __init__(
+        self,
+        *,
+        token: SecretStr,
+        config_id: str | None = None,
+        domain: str = "aws.us.pangea.cloud",
+        log_missing_parent: bool = False,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            token: Pangea Secure Audit Log API token.
+            config_id: Pangea Secure Audit Log configuration ID.
+            domain: Pangea API domain.
+        """
+
+        super().__init__(**kwargs)
+        self.log_missing_parent = log_missing_parent
+        self._client = Audit(token=token.get_secret_value(), config=PangeaConfig(domain=domain), config_id=config_id)
+
+    @override
+    def _persist_run(self, run: Run) -> None:
+        pass
+
+    @override
+    def _on_llm_start(self, run: Run) -> None:
+        inputs = {"prompts": [p.strip() for p in run.inputs["prompts"]]} if "prompts" in run.inputs else run.inputs
+        self._client.log_bulk(
+            [
+                {
+                    "timestamp": run.start_time,
+                    "action": "llm/start",
+                    "target": run.name,
+                    "message": "Starting LLM.",
+                    "new": canonicalize_json(inputs).decode("utf-8"),
+                }
+            ]
+        )
